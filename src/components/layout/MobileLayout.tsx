9182059fcbf377d83e00c067989cc225bf912951
WilliamKerridge <will1kerridge@gmail.com>
2026-03-11 10:28:44 +0000

    feat: move tab bar to fixed bottom position in MobileLayout

--- src/components/layout/MobileLayout.tsx
+++ src/components/layout/MobileLayout.tsx
@@ -10,11 +10,17 @@ interface Props {
 export default function MobileLayout({ header, modeSelector, nudge, content }: Props) {
   return (
     <div className="min-h-screen">
-      <div className="max-w-md mx-auto px-4 pb-8 flex flex-col min-h-screen">
+      <div className="max-w-md mx-auto px-4 flex flex-col min-h-screen pb-[95px]">
         {header}
-        {modeSelector}
         {nudge}
         <div className="flex-1 mt-6">{content}</div>
+      </div>
+
+      {/* Fixed floating bottom tab bar */}
+      <div className="fixed bottom-0 left-0 right-0 z-50 flex justify-center pointer-events-none">
+        <div className="w-full max-w-md px-5 pb-5 pt-3 pointer-events-auto">
+          {modeSelector}
+        </div>
       </div>
     </div>
   )
